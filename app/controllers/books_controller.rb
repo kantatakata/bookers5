68111c443eab1e4c6010147dbc91568fa69085c9
kantatakata <112669597+kantatakata@users.noreply.github.com>
2022-10-03 09:38:25 +0000

    [finish]応用課題５

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 before_action :authenticate_user!
 before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def show
-    @book = Book.find
+    @book = Book.find(params[:id])
     @book_new = Book.new
   end
 
